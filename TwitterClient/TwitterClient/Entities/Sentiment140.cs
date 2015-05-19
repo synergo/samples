5190b820e0a9e4867d3757cd77667e8bd5c74385
Dipanjan Banik <dipanb@microsoft.com>
2015-05-18 17:19:17 -0700

    Changed the MSFT License

--- TwitterClient/TwitterClient/Entities/Sentiment140.cs
+++ TwitterClient/TwitterClient/Entities/Sentiment140.cs
@@ -1,8 +1,13 @@
-﻿//------------------------------------------------------------------------------ 
-// <copyright> 
-//     Copyright (c) Microsoft Corporation. All Rights Reserved. 
-// </copyright> 
-//------------------------------------------------------------------------------ 
+﻿//********************************************************* 
+// 
+//    Copyright (c) Microsoft. All rights reserved. 
+//    This code is licensed under the Microsoft Public License. 
+//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF 
+//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY 
+//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR 
+//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT. 
+// 
+//*********************************************************
 
 using Newtonsoft.Json.Linq;
 using System;
