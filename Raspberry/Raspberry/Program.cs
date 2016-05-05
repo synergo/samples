80e3ef45ae2180519687b643fc0e72670efc5141
Madalin Plastoi <madalin.plastoi@synergogroup.net>
2016-05-05 18:56:19 +0300

    added our own simulator

---
+++ Raspberry/Raspberry/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace Raspberry
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=SynergoSmartThings.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Hu8BkBEyuDKLagqNwxUqOySGuvWGC7VpERISZ44poco=";
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "myFirstDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
